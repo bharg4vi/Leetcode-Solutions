63b2084beab0f9594ebb934742412dfd4bb1abd3
Bhargavi <80198976+bharg4vi@users.noreply.github.com>
2022-09-26 18:59:49 +0530

    Create removeNthNode.java

---
+++ ALGORITHM STUDY PLAN/removeNthNode.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        for(int i=0;i<n;i++){
+            fast = fast.next;
+        }
+        
+        if(fast == null){
+            head = head.next;
+            return head;
+        }
+        
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+    
+}
