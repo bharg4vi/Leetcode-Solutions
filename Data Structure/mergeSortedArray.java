5520c0b4c3852f2065f56c351eed6111529407a7
Bhargavi <80198976+bharg4vi@users.noreply.github.com>
2022-09-27 11:17:16 +0530

    Create mergeSortedArray.java

---
+++ Data Structure/mergeSortedArray.java
@@ -0,0 +1,19 @@
+class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    int i1 = m-1;
+    int i2 = n-1;
+    
+    for(int i=m+n-1; i>=0;i--){
+      
+      if(i2 < 0)
+        return;
+      
+      if(i1 >=0 && nums1[i1] > nums2[i2])
+        nums1[i] = nums1[i1--];
+      else
+        nums1[i] = nums2[i2--];
+      
+    }
+  }
+}
